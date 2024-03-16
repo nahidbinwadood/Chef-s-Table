cabfbb76469b97fc57669c1e79c16f7e90f4dcb3
nahidbinwadood <nahidbinwadood@gmail.com>
2024-03-16 15:40:35 +0600

    added all ingredients

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,23 +1,31 @@
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 import img2 from './img/fire.png'
 import img3 from './img/Frame.png'
-const Recipe = ({recipe}) => {
-    const {recipe_name,recipe_image,short_description,ingredients,preparing_time,calories}=recipe;
-    console.log(ingredients.map((item)=> item));
+const Recipe = ({ recipe }) => {
+    const { recipe_name, recipe_image, short_description, ingredients, preparing_time, calories } = recipe;
     return (
         <div className='space-y-6 p-6 border border-gray-400 rounded-2xl'>
             <div className='object-center'>
                 <img className='w-96 h-60 rounded-xl ' src={recipe_image} alt="" />
             </div>
-            <h2 className='text-xl font-semibold'>{recipe_name}</h2>
-            <p className='text-[#878787]'>{short_description} </p>
+            <div>
+                <h2 className='text-xl font-semibold'>{recipe_name}</h2>
+                <p className='text-[#878787]'>{short_description} </p>
+            </div>
+
             <div className='border border-[#30325E33]'></div>
-            <h2 className='text-[#282828] font-semibold text-lg'>Ingredients: {ingredients.length} </h2>
-            <ul className='text-[#878787]'>
-                <li>500g ground beef</li>
-                <li>1 onion,chopped</li>
-                <li>2 cloves garlic,minced</li>
-            </ul>
+            <div className='lg:h-56'>
+                <h2 className='text-[#282828] font-semibold text-lg'>Ingredients: {ingredients.length} </h2>
+                <ul className="list-disc list-inside">
+                    {
+                        ingredients.map((item, idx) => (
+                            <li key={idx}>{item} </li>
+                        )
+                        )
+                    }
+                </ul>
+            </div>
+
             <div className='border border-[#30325E33]'></div>
             <div className='flex gap-12 text-[#878787]'>
                 <div className='flex items-center justify-center gap-2'>
@@ -35,7 +43,7 @@ const Recipe = ({recipe}) => {
 };
 
 
-Recipe.propTypes={
-    recipe:PropTypes.object.isRequired
+Recipe.propTypes = {
+    recipe: PropTypes.object.isRequired
 }
 export default Recipe;
